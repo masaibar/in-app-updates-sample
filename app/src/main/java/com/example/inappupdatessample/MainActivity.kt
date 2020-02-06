fe333c5956daa1b3368814ac04599e363d74cba0
masaibar <masaibar.dev@gmail.com>
2020-02-06 16:47:35 +0900

    不要だが共通部分としてInstallStateUpdatedListenerに関する記述を追加

--- app/src/main/java/com/example/inappupdatessample/MainActivity.kt
+++ app/src/main/java/com/example/inappupdatessample/MainActivity.kt
@@ -6,11 +6,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallState
+import com.google.android.play.core.install.InstallStateUpdatedListener
 import com.google.android.play.core.install.model.ActivityResult
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.UpdateAvailability
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), InstallStateUpdatedListener {
 
     companion object {
         private const val REQUEST_CODE_IMMEDIATE = 5678
@@ -24,6 +26,9 @@ class MainActivity : AppCompatActivity() {
 
         // 初期化する
         appUpdateManager = AppUpdateManagerFactory.create(this)
+
+        // インストール状況の更新を受け取れるようにする(Immediateでは不要)
+        appUpdateManager.registerListener(this)
 
         // アップデート有無を確認し、成功時のみ処理をする
         appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
@@ -56,6 +61,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onDestroy() {
+        appUpdateManager.unregisterListener(this)
+        super.onDestroy()
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE_IMMEDIATE) {
@@ -71,5 +81,9 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onStateUpdate(state: InstallState?) {
+        // Immediateでは不要
     }
 }
