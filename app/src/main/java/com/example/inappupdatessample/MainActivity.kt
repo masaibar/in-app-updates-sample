5440b9e288719784f951e13a9ce56592f009b998
masaibar <masaibar.dev@gmail.com>
2020-02-06 16:22:31 +0900

    Flexibleアップデートのサンプル

--- app/src/main/java/com/example/inappupdatessample/MainActivity.kt
+++ app/src/main/java/com/example/inappupdatessample/MainActivity.kt
@@ -1,12 +1,96 @@
 package com.example.inappupdatessample
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallState
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.ActivityResult
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), InstallStateUpdatedListener {
+
+    companion object {
+        private const val REQUEST_CODE_FLEXIBLE = 1234
+    }
+
+    private lateinit var appUpdateManager: AppUpdateManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+        appUpdateManager.registerListener(this)
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE &&
+                appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
+            ) {
+                // Flexibleアップデートを実行する
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    AppUpdateType.FLEXIBLE,
+                    this,
+                    REQUEST_CODE_FLEXIBLE
+                )
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                // ユーザーに通知する
+                showDownloadedSnackbar()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        appUpdateManager.unregisterListener(this)
+        super.onDestroy()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_FLEXIBLE) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+
+                }
+                Activity.RESULT_CANCELED -> {
+
+                }
+                ActivityResult.RESULT_IN_APP_UPDATE_FAILED -> {
+
+                }
+            }
+        }
+    }
+
+    override fun onStateUpdate(state: InstallState?) {
+        if (state?.installStatus() == InstallStatus.DOWNLOADED) {
+            showDownloadedSnackbar()
+        }
+    }
+
+    private fun showDownloadedSnackbar() {
+        Snackbar.make(
+            findViewById(R.id.layout_root),
+            "ダウンロードが完了しました",
+            Snackbar.LENGTH_INDEFINITE
+        ).run {
+            setAction("更新する") {
+                appUpdateManager.completeUpdate()
+            }
+            show()
+        }
     }
 }
